2c17c3d3a524646c090c2c45051f03376398a877
LinKuangren <romaindanne@gmail.com>
2023-01-12 12:21:45 +0100

    feat(#24) ajout départ et arrivé all roadtrip

--- template/RoadTrip/index.php
+++ template/RoadTrip/index.php
@@ -1,13 +1,25 @@
 <div class="grid grid-cols-4 gap-4 mt-5">
-    <?php foreach($data['listeRoadTrip'] as $currentRoadTrip) { ?>
+    <?php foreach($data['listeRoadTrip'] as $index => $currentRoadTrip) { ?>
+        <?php $checkpoint = $currentRoadTrip->getAllCheckpoint($currentRoadTrip->getId()); ?>
         <div class="card w-46 bg-base-100 shadow-xl">
             <div class="card-body">
                 <a href="/index.php?page=detailsRoadTrip&id=<?= $currentRoadTrip->getId() ?>">
                     <img alt="Placeholder" class="block h-auto w-full" src="<?= $currentRoadTrip->getImage() != null ? $currentRoadTrip->getImage() : "https://picsum.photos/1920/1080/?random" ?>">
                 </a>
                 <h2 class="card-title"><?= $currentRoadTrip->getIntitule() ?></h2>
-                <p><?= $currentRoadTrip->getDescription() ?></p>
                 <h3><?= $currentRoadTrip->getUser()->getEmail() ?></h3>
+                <?php foreach($checkpoint as $index => $currentCheckPoint) { ?>
+                    <?php if($index == array_key_first($checkpoint)) { ?>
+                        <h3><strong>Adresse</strong> : <?= $currentCheckPoint->getCoordonnee() ?></h3>
+                        <p><strong>Date de départ</strong> : <?= $currentCheckPoint->getDateDepart() ?></p>
+                    <?php } ?>
+                    <?php if($index == array_key_last($checkpoint)) { ?>
+                            <h3><strong>Adresse</strong> : <?= $currentCheckPoint->getCoordonnee() ?></h3>
+                            <p><strong>Date d'arrivée</strong> : <?= $currentCheckPoint->getDateArrive() ?></p>
+                    <?php } ?>
+                <?php } ?>
+                <p><?= $currentRoadTrip->getDescription() ?></p>
+
                 <div class="flex justify-end gap-5">
                     <?php if(isset($_SESSION['user']) && $_SESSION['user']['id'] == $currentRoadTrip->getUser()->getId()){ ?>
                         <div class="card-actions">
