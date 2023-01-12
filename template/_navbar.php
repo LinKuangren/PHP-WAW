7cb12d868d0927de695963aa5e90d4d40f7cab57
De-lourdes <abboetmado@gmail.com>
2023-01-12 16:56:40 +0100

    feat(#3): js menu burger navbar

--- template/_navbar.php
+++ template/_navbar.php
@@ -1,43 +1,53 @@
 <header>
-    <nav class="navbar bg-[#FEEFDD] text-black shadow-lg">
-        <div class="navbar-start">
-            <a class="btn btn-ghost normal-case text-xl">WaW</a>
-            <div class="dropdown">
-                <label tabindex="0" class="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-            </div>
-            
+    <nav class="p-5 bg-[#FEEFDD] shadow md:flex md:items-center md:justify-between">
+        <div class="flex justify-between items-center ">
+            <span class="text-2xl text-black font-[Poppins] cursor-pointer">
+                <img class="h-10 inline"
+                src="https://tailwindcss.com/_next/static/media/social-square.b622e290e82093c36cca57092ffe494f.jpg">
+                WaW
+            </span>
+
+            <span class="text-3xl text-black cursor-pointer mx-2 md:hidden block">
+                <ion-icon name="menu" onclick="Menu(this)"></ion-icon>
+            </span>
         </div>
-        <div class="navbar-end hidden lg:flex">
-            <ul class="menu menu-horizontal p-0">
-                <li class="hover:text-[#6699CC]">
-                    <a href="index.php?page=home">Accueil</a>
+
+        <ul class="md:flex md:items-center z-[10] md:z-auto md:static absolute md:bg-[#FEEFDD] bg-[#000] max-[720px]:opacity-70 w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+            <li class="mx-4 my-6 md:my-0">
+                <a href="index?page=home" class="text-xl max-[720px]:text-white text-black hover:text-[#6699CC] duration-500">Accueil</a>
+            </li>
+            <li class="mx-4 my-6 md:my-0">
+                <a href="index?page=allRoadTrip" class="text-xl max-[720px]:text-white text-black hover:text-[#6699CC] duration-500">Liste des road trips</a>
+            </li>
+            <?php if(!isset($_SESSION['user'])){ ?>
+                <li class="mx-4 my-6 md:my-0">
+                    <a href="index?page=register" class="max-[720px]:text-white text-black text-xl hover:text-[#6699CC] duration-500">S'enregistrer</a>
                 </li>
-                <li class="hover:text-[#6699CC]">
-                    <a href="index.php?page=allRoadTrip">Liste des Road Trip</a>
+                <li class="mx-4 my-6 md:my-0">
+                    <a href="index?page=login" class="max-[720px]:text-white text-black text-xl hover:text-[#6699CC] duration-500">Se connecter</a>
                 </li>
-                <?php if(!isset($_SESSION['user'])){ ?>
-                    <li class="hover:text-[#6699CC]">
-                        <a href="index.php?page=register">S'enregistrer</a>
-                    </li>
-                    <li class="hover:text-[#6699CC]">
-                        <a href="index.php?page=login">Se connecter</a>
-                    </li>
-                <?php }else{ ?>
-                    <li class="hover:text-[#6699CC]">
-                        <a href="index.php?page=addRoadTrip">Créer un road trip</a>
-                    </li>
-                    <li class="hover:text-[#6699CC]">
-                        <a href="index.php?page=logout">Deconnexion</a>
-                    </li>
-                <?php } ?>
-            </ul>
-        </div>
-        <?php if(isset($_SESSION['user'])){ ?>
-            <div class="navbar-end">
-                <a href="index.php?page=user_profile" class="btn">Mon Profil</a>
-            </div>
-        <?php } ?>
+            <?php } else{ ?>  
+            <li class="mx-4 my-6 md:my-0">
+                <a href="index?page=addRoadTrip" class="max-[720px]:text-white text-black text-xl hover:text-[#6699CC] duration-500">Créer un road trip</a>
+            </li>
+            <li class="mx-4 my-6 md:my-0">
+                <a href="index?page=logout" class="max-[720px]:text-white text-black text-xl hover:text-[#6699CC] duration-500">Déconnexion</a>
+            </li>
+            <?php } ?>
+            <?php if(isset($_SESSION['user'])){ ?>
+                <li>
+                    <a href="index?page=profil" class="bg-[#6699CC] text-white font-[Poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 rounded ">
+                    Mon profil
+                    </a>
+                </li>
+            <?php } ?>
+        </ul>
+    
     </nav>
+    <script>
+        function Menu(e){
+            let list = document.querySelector('ul');
+            e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+        }
+    </script>
 </header>
