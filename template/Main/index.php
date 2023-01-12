cc2db2bc25b8668cb6c3c23ce6a3a66a4959a11c
De-lourdes <abboetmado@gmail.com>
2023-01-12 13:08:33 +0100

    feat(#19): ajoute style de la page d'accueil v1

--- template/Main/index.php
+++ template/Main/index.php
@@ -1,3 +1,30 @@
-<h1 class="text-8xl text-black text-center">Bonjour</h1>
-
-
+<main class="text-[#161616] ">
+        <div class="page_de_garde h-screen flex items-center justify-center text-center bg-[url('https://picsum.photos/200')]" >
+            <h1 class="text-lg font-black">L'expérience Where are we (WaW), <br/> c'est ici et maintenant</h1>
+        </div>
+        <div class="flex flex-col items-center justify-center p-10 gap-5">
+            <h1>C'est quoi WaW?</h1>
+            <p>L’agence de communication « New Horizons » vous proposes une plateforme, nommée « Waw » (pour « Where Are We »). Entièrement gratuite, cette plateforme permet dans un premier temps de rassembler une communauté d’adeptes des road trips qui aime partager leurs road trips sur les réseaux sociaux et notamment Instagram pour leur cercle personnel (famille, amis), à travers un compte dédié à ce voyage.</p>
+        </div>
+        <div class="container_bloc2">
+            
+            <div class="flex flex-col items-center justify-center p-10 gap-5">
+                <h1>Comment cela fonctionne?</h1>
+                <img src="image/kevin-schmid-Mta8r0bxhbo-unsplash-scaled.jpg" alt="">
+            </div>
+            <div class="container_fonctionnement">
+                <div class="flex flex-col items-center justify-center p-10 gap-5">
+                    <h1>Créer un compte sur WaW</h1>
+                    <p>Inscrivez-vous sur le site pour participer à cette incroyable aventure deu partage des road trip.</p>
+                </div>
+                <div class="flex flex-col items-center justify-center p-10 gap-5">
+                    <h1>Créer un road trip à partager</h1>
+                    <p>Après cet inscrit sur le site cliqué sur le bouton créé un Road Trip est inséré les adresses de départ et d'arrivée dans la première étape les date de départ et d'arrivée avec le véhicule du voyage dans la deuxième étape et pour la troisième étape le nom du Road Trip et la photo de couverture.</p>
+                </div>
+                <div class="flex flex-col items-center justify-center p-10 gap-5">
+                    <h1>Modifier les road trip en cours</h1>
+                    <p>Vous avez changé d'itinéraire ou vous avez prolongé votre Road Trip en passant par des correspondances entre-temps . Pas de soucis vous pouvez modifier le road trip déjà en ligne !</p>
+                </div>
+            </div>
+        </div>
+</main>
